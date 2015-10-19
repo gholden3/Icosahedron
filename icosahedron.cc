ea46a03d7016e2ce6b29c96161f1f4514b63ef7a
Gina Marie Holden <gholden3@deepthought19.cc.gatech.edu>
2015-10-19 13:16:19 -0400

    test cases 1-6: rotation, recursive subdivisions from command line, but still aliasing on rotation meep.

--- icosahedron.cc
+++ icosahedron.cc
@@ -1,6 +1,6 @@
 // Draw an Icosahedron
 // ECE4893/8893 Project 4
-// YOUR NAME HERE
+// Gina Holden
 
 #include <iostream>
 #include <math.h>
@@ -24,13 +24,20 @@ using namespace std;
 #define Z .850650808352039932
 
 void drawIcosahedron(int);
-
+void normalize(GLfloat *);
 // These are the 12 vertices for the icosahedron
 static GLfloat vdata[NVERTEX][3] = {    
    {-X, 0.0, Z}, {X, 0.0, Z}, {-X, 0.0, -Z}, {X, 0.0, -Z},    
    {0.0, Z, X}, {0.0, Z, -X}, {0.0, -Z, X}, {0.0, -Z, -X},    
    {Z, X, 0.0}, {-Z, X, 0.0}, {Z, -X, 0.0}, {-Z, -X, 0.0} 
 };
+
+GLfloat rotY = 0.0; //starting rotation offset for each direction
+GLfloat rotZ = 0.0;
+GLfloat rotX = 0.0;
+GLfloat dx = 1.0;//for rotation tests, rotate 1 deg/update around x and y
+GLfloat dy = 1.0;
+GLfloat dz = 0.0;//not z
 
 // These are the 20 faces.  Each of the three entries for each 
 // vertex gives the 3 vertices that make the face.
@@ -45,17 +52,20 @@ int divisions = 0;
 // Test cases.  Fill in your code for each test case
 void Test1()
 {
-  cout << "hello from Test1()" << endl;
+  //cout << "hello from Test1()" << endl;
   drawIcosahedron(0);
 //  glFlush();
 }
 
 void Test2()
 {
+  //cout << "hello from Test2()" << endl;
+  drawIcosahedron(0);
 }
 
 void Test3()
 {
+  drawIcosahedron(1);
 }
 
 void Test4()
@@ -64,10 +74,12 @@ void Test4()
 
 void Test5(int depth)
 {
+  drawIcosahedron(depth);
 }
 
 void Test6(int depth)
 {
+
 }
 
 void drawTriangle(GLfloat* v1, GLfloat* v2, GLfloat* v3)
@@ -89,10 +101,27 @@ void drawTriangle(GLfloat* v1, GLfloat* v2, GLfloat* v3)
 
 void subDivide(GLfloat* v1, GLfloat* v2, GLfloat * v3, int depth){
   if(depth==0){
-     cout << "hello from subdivide. depth = "<< depth << endl;
+    // cout << "hello from subdivide. depth = "<< depth << endl;
     drawTriangle(v1,v2,v3);
     return;
   }
+  //find the midpoints
+  GLfloat v12[3];
+  GLfloat v23[3];
+  GLfloat v31[3];
+  for(int i = 0; i<3; i++){//normalize takes care of the length of these
+    v12[i] = v1[i] + v2[i];
+    v23[i] = v2[i] + v3[i];
+    v31[i] = v3[i] + v1[i];
+  }
+  normalize(v12);
+  normalize(v23);
+  normalize(v31);
+  //draw the four smaller traingles
+  subDivide(v1,v12,v31,depth-1);
+  subDivide(v2,v23,v12,depth-1);
+  subDivide(v3,v31,v23,depth-1);
+  subDivide(v12,v23,v31,depth-1);
 }
 
 void drawIcosahedron(int maxDepth){
@@ -111,13 +140,15 @@ void display(void)
   // clear al
   glEnable(GL_LINE_SMOOTH);
   glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_DEPTH_BUFFER_BIT);
+  glEnable(GL_DEPTH_TEST);
   glColor3f(1.0,1.0,1.0);
   // Clear the matrix
   glLoadIdentity();
   // Set the viewing transformation
   gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
   glTranslatef(250, 250, 0);
-  glScalef(100.0, 100.0, 0);
+  glScalef(200.0, 200.0, 0);
   //drawTriangle();
   switch(testNumber)
     {
@@ -127,20 +158,33 @@ void display(void)
         //drawIcosahedron(0);
        }
         break;
-      case 2 : 
-        Test2();
+      case 2 : {
+        glRotatef(rotX,rotY, rotZ,1.0);//perform rotation
+        Test2();//draw
+        rotX += dx; //update rotation
+        rotY += dy;
+        //rotZ += dz;
+        }
         break;
       case 3 : 
         Test3();
         break;
-      case 4 :
-        Test4();
+      case 4 :{
+        glRotatef(rotX,rotY,rotZ,1.0);
+        Test3();
+        rotX += dx;
+        rotY += dy;
+        }
         break;
       case 5 :
         Test5(divisions);
         break;
-      case 6 :
-        Test6(divisions);
+      case 6 :{
+        glRotatef(rotX,rotY,rotX,1.0);
+        Test5(divisions); //same as test 5, but add rotations
+        rotX += dx;
+        rotY += dy;
+        }
         break;
     }
   // Flush buffer
@@ -165,7 +209,7 @@ void normalize(GLfloat v[3]){
 
 
 void reshape(int w, int h)
-{
+{ //unmodified from given reshape function
   glViewport(0,0, (GLsizei)w, (GLsizei)h);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
@@ -175,9 +219,9 @@ void reshape(int w, int h)
 }
 
 
-void timer(int){
+void timer(int){ //unmodified from given timer func, except set time to 100ms
   glutPostRedisplay();
-  glutTimerFunc(25.0,timer,0);
+  glutTimerFunc(100,timer,0);
 }
 
 
@@ -203,7 +247,7 @@ int main(int argc, char** argv)
   // Set your glut callbacks here
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
-  glutTimerFunc(25.0, timer, 0);
+  glutTimerFunc(100, timer, 0);//timer is update rate = 100ms
   // Enter the glut main loop here
   glutMainLoop();
   return 0;
